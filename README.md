f4b2e16b2c4a0b5169fc25d4194df5f8b712c66d
Harwex213 <harwex213@mail.ru>
2023-11-17 21:08:16 +0300

    Note TS config

---
+++ README.md
@@ -0,0 +1,3 @@
+# Harwex Notes
+
+Repository with notes that have been written during exploration and learning
---
+++ typescript/src/decorators.ts
@@ -0,0 +1,3 @@
+// Complete guide to Decorators (stage 2) - https://mirone.me/a-complete-guide-to-typescript-decorator
+// Microsoft decorators (stage 3) introduction - https://devblogs.microsoft.com/typescript/announcing-typescript-5-0/#decorators
+
