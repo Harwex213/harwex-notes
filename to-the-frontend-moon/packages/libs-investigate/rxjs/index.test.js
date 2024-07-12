28e1b5f04d0107f43060e241108c0e83135f0b12
Aleh Kaportsau <aleh.kaportsau@softnetix.io>
2024-07-12 12:10:19 +0300

    note about rxjs: concat & merge

--- to-the-frontend-moon/packages/libs-investigate/rxjs/index.test.js
+++ to-the-frontend-moon/packages/libs-investigate/rxjs/index.test.js
@@ -1,6 +1,6 @@
 import test from "node:test";
 import assert from "node:assert";
-import { Subject, map } from "rxjs"
+import {Subject, map, concat, interval, takeWhile, merge} from "rxjs"
 
 test("Subject should have multiple subscribers", (t, done) => {
     const subject = new Subject();
@@ -43,4 +43,48 @@ test("Subject should can be piped", (t, done) => {
     piped.next(emitted);
     console.log("piped.complete");
     piped.complete();
+});
+
+test("Concat should process observables sequentially", (t, done) => {
+    const source$1 = interval(10).pipe(takeWhile(value => value < 3));
+    const source$2 = interval(5).pipe(map(value => value + 5), takeWhile(value => value < 8));
+    const result = concat(
+        source$1,
+        source$2,
+    );
+
+    const expected = [0, 1, 2, 5, 6, 7];
+    const actual = [];
+    result.subscribe({
+        next: (value) => {
+            actual.push(value);
+            console.log("currentValue", value);
+        },
+        complete: () => {
+            assert.deepStrictEqual(expected, actual);
+            done();
+        }
+    });
+});
+
+test("Merge should process observables concurrently", (t, done) => {
+    const source$1 = interval(30).pipe(takeWhile(value => value < 3));
+    const source$2 = interval(5).pipe(map(value => value + 5), takeWhile(value => value < 8));
+    const result = merge(
+        source$1,
+        source$2,
+    );
+
+    const expected = [5, 6, 7, 0, 1, 2];
+    const actual = [];
+    result.subscribe({
+        next: (value) => {
+            actual.push(value);
+            console.log("currentValue", value);
+        },
+        complete: () => {
+            assert.deepStrictEqual(expected, actual);
+            done();
+        }
+    });
 });
